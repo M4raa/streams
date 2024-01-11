4201ef642d3c395f646510934483a8d91ddd37f8
M4raa <davidmarpons@gmail.com>
2024-01-11 11:15:50 +0100

    Update

---
+++ src/Human.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Human implements Comparable<Human>{
+    private String name;
+    private double salario;
+
+    public Human(String name, double salario){
+        this.name = name;
+        this.salario = salario;
+    }
+
+    public String getName() { return this.name; }
+
+    public double getSalario() { return this.salario; }
+    @Override
+    public int compareTo(Human other) {
+        if (this.salario == other.getSalario()) {
+            return 0;
+        } else if (this.salario> other.getSalario()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
